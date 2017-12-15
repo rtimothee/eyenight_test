49e4d965673f4474291640ceb5338ab1da86f45c
Roldao Timothée <troldao@voyages-sncf.com>
2017-12-15 14:01:44 +0100

    Add Twig structure and CGV page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
     }
+
+
+    /**
+     * @Route("/cgi", name="cgi")
+     */
+    public function cgiAction(){
+        return $this->render('default/cgi.html.twig', []);
+    }
 }
